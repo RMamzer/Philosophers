534aae609658d0f829a7686c8fcbe53afeccfa46
Roman Mamzer <mamzer.roman@gmail.com>
2025-12-26 20:32:46 +0200

    added  struct initialization, completed parsing.

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/25 18:02:42 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/12/25 19:45:57 by rmamzer          ###   ########.fr       */
+/*   Updated: 2025/12/26 19:54:08 by rmamzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,41 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <limits.h>
 
 # define SUCCESS 0
 # define FAILURE 1
 
-#define ERROR_ARGSN "Wrong number of arguments\nUsage: [number_of_philosophers]\
+# define ERROR_ARGSN "Wrong number of arguments\nUsage: [number_of_philosophers]\
  [time_to_die] [time_to_eat] [time_to_sleep] (optional\
 [number_of_times_each_philosopher_must_eat])\n"
-#define ERROR_ARGEMPTY "Error: empty argument\n"
-#define ERROR_ARG "Error: only digits allowed\n"
+# define ERROR_ARGEMPTY "Error: empty argument\n"
+# define ERROR_ARG "Error: non-digit input\n"
+# define ERROR_INTOVRF "Error: number is too large\n"
+# define ERROR_ARGZERO "Error: number cannot be 0\n"
+# define ERROR_MEM "Error: memory allocation failure\n"
+
+
+typedef struct s_philo
+{
+	int id;
+} t_philo;
+
+
+
+
+typedef struct s_academy
+{
+	int philos_total;
+	int time_to_die;
+	int time_to_sleep;
+	int time_to_eat;
+	int num_of_meals;
+
+	t_philo			*philo;
+	pthread_mutex_t	*fork;
+
+} t_academy;
 
 
 #endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,12 +6,21 @@
 /*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/25 18:02:30 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/12/25 19:40:55 by rmamzer          ###   ########.fr       */
+/*   Updated: 2025/12/26 20:32:09 by rmamzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "philo.h"
+
+
+void free_tablle(t_academy *table)
+{
+	if(table->philo)
+		free(table->philo);
+	if(table->fork)
+		free(table->fork);
+}
 
 int	ft_strlen(const char *str)
 {
@@ -41,8 +50,6 @@ void write_error_msg(char *msg)
 	write (STDERR_FILENO, msg, ft_strlen(msg));
 }
 
-
-
 bool check_arg_num(int argc)
 {
 	if (argc !=5 && argc !=6)
@@ -50,6 +57,29 @@ bool check_arg_num(int argc)
 	return (SUCCESS);
 
 }
+
+
+bool	ft_check_overflow_and_zero(char *nptr)
+{
+	int		i;
+	long	num;
+
+	i = 0;
+	num = 0;
+	while (ft_isspace(nptr[i]))
+		i++;
+	while (ft_isdigit(nptr[i]))
+	{
+		num = num * 10 + (nptr[i] - '0');
+		if (num >INT_MAX)
+			return(write_error_msg(ERROR_INTOVRF), FAILURE);
+		i++;
+	}
+	if (num == 0)
+		return(write_error_msg(ERROR_ARGZERO), FAILURE);
+	return (SUCCESS);
+}
+
 
 bool check_input(int argc, char **argv)
 {
@@ -70,13 +100,88 @@ bool check_input(int argc, char **argv)
 			j++;
 		if (argv[i][j] != '\0')
 			return (write_error_msg(ERROR_ARG), FAILURE);
+		if (ft_check_overflow_and_zero(argv[i]))
+			return (FAILURE);
 	}
 	return (SUCCESS);
 }
 
-int main(int argc, char **argv)
+
+int philo_atoi(const char *nstr)
 {
+	int	num;
+	int		i;
+
+	i = 0;
+	num = 0;
+	while(ft_isspace(nstr[i]))
+		i++;
+	while(ft_isdigit(nstr[i]))
+	{
+		num = num * 10 + (nstr[i] - '0');
+		i++;
+	}
+	return(num);
+}
+
+
+
+bool	init_table(int argc, char **argv, t_academy *table)
+{
+	table->philo = NULL;
+	table->fork = NULL;
+	table->philos_total = philo_atoi(argv[1]);
+	table->time_to_die = philo_atoi(argv[2]);
+	table->time_to_eat = philo_atoi(argv[3]);
+	table->time_to_sleep = philo_atoi(argv[4]);
+	if (argc ==  6)
+		table->num_of_meals = philo_atoi(argv[5]);
+	else
+		table->num_of_meals = 0;
+	table->philo == malloc(sizeof(t_philo) * table->philos_total);
+		if(!table->philo)
+			return(write_error_msg(ERROR_MEM),FAILURE);
+	table->fork == malloc(sizeof(t_philo) * table->philos_total);
+		if(!table->fork)
+			return(write_error_msg(ERROR_MEM),FAILURE);
+	return(SUCCESS);
+}
+
+
+bool init_mutexes(t_academy *table)
+{
+	int	i;
+
+	i = 0;
+	while (i < table->philos_total)
+	{
+
+	}
+
+
+	return (SUCCESS);
+}
+
+
+
+bool init_data( int argc, char **argv, t_academy *table)
+{
+	if (init_table(argc, argv, table)== FAILURE)
+		return (free_table(table), FAILURE);
+	if (init_mutexes(table)== FAILURE)
+		return(free_table(table), FAILURE);
+	return (SUCCESS);
+
+}
+
+
+int	main(int argc, char **argv)
+{
+	t_academy table;
+
 	if (check_input(argc, argv) == FAILURE)
+		return(FAILURE);
+	if (init_data(argc, argv, &table) == FAILURE)
 		return(FAILURE);
 	return (SUCCESS);
 }
